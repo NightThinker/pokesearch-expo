9ac181a03f8b0fdf2ff541968d0fa255db0e0b90
HDT <hdt@MacBook-Air-khxng-HDT.local>
2018-07-16 16:50:35 +0700

    handling Platform

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
@@ -11,11 +11,10 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === 'android' ? 24 : 40,
   },
-});
+};
