e41fddfb8adacc9c02d0c893c7497320c186a248
HDT <hdt@MacBook-Air-khxng-HDT.local>
2018-07-16 21:07:19 +0700

    style native-base basic

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Platform, Text, View } from 'react-native';
+import { Button } from 'native-base';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Button><Text>Hello</Text></Button>
       </View>
     );
   }
