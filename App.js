86d1f642eda3587a5d8aa13ecf2932aaf3eac06b
HDT <hdt@MacBook-Air-khxng-HDT.local>
2018-07-16 21:51:59 +0700

    setting up landing page

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Platform, Text, View } from 'react-native';
+import { Platform, Text, View, ImageBackground } from 'react-native';
 import { Button } from 'native-base';
+
+const myBackgroud = require('./assets/landing.jpg')
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Button><Text>Hello</Text></Button>
+        <ImageBackground source={myBackgroud } style={{width: '100%', height: '100%'}} >
+          <View style={styles.viewStyle}>
+            <Text style={styles.titleStyle}>Welcome to PokeSearch</Text>
+            <Button 
+              block={true}
+              style={styles.buttonStyle}
+              onPress={() => {}}
+            >
+              <Text style={styles.buttonText}>Start Searching...</Text>
+            </Button>
+          </View>
+        </ImageBackground>
       </View>
     );
   }
@@ -19,4 +31,21 @@ const styles = {
     backgroundColor: '#fff',
     marginTop: Platform.OS === 'android' ? 24 : 40,
   },
+  viewStyle: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleStyle: {
+    fontSize: 30,
+    color: 'blue',
+    alignItems: 'center',
+  },
+  buttonStyle: {
+    margin: 10,
+  },
+  buttonText: {
+    color: 'white'
+  }
 };
