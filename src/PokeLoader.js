3f293f9dcd33bb3f5a2d45d083cb037b483b7347
jiraphon <jiraphon@homedottech.com>
2018-07-18 15:03:56 +0700

    loading gif

--- src/PokeLoader.js
+++ src/PokeLoader.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 
 class PokeLoader extends React.Component {
     render() {
         return (
             <View>
-                <Text>We are Loading</Text>
+                <Image 
+                    source={{uri: 'https://media.tenor.com/images/39d6060576a516f1dd437eafccafbdb1/tenor.gif'}}
+                    style={styles.img}
+                />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    img: {
+        height: 400,
+        width: 400,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
 
 export default PokeLoader
