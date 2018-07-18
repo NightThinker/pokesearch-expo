81b7162ad6ad22061f2e85f5e53cb95f52fa596a
jiraphon <jiraphon@homedottech.com>
2018-07-18 16:15:46 +0700

    add background

--- src/SearchBody.js
+++ src/SearchBody.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { ScrollView, Text, View, Image } from 'react-native'
+import { ScrollView, Text, View, Image, ImageBackground, Dimensions } from 'react-native'
 import { ListItem, List } from 'native-base';
+
+const height = Dimensions.get('window').height
+const width = Dimensions.get('window').width
+
 
 class SearchBody extends React.Component {
     render() {
@@ -9,44 +13,55 @@ class SearchBody extends React.Component {
             return <View/>
         }
         return (
-            <ScrollView style={{flex: 1}}>
-                <Text style={styles.header}>#{pokeData.id} - {pokeData.name.toUpperCase()}</Text>
-                <View style={styles.viewStyle}>
-                    <Image 
-                        source={{uri: pokeData.sprites.front_default}}
-                        style={styles.img}
-                    />
-                </View>
-                <View style={styles.info}>
-                    <ListItem itemDivider>
-                        <Text style={{fontWeight: 'bold'}}>Size</Text>
-                    </ListItem>
-                    <ListItem >
-                        <Text >Weight - {pokeData.weight} kg</Text>
-                    </ListItem>
-                    <ListItem >
-                        <Text >Height - {pokeData.height / 10} m</Text>
-                    </ListItem>
-                    <ListItem itemDivider>
-                        <Text style={{fontWeight: 'bold'}}>Abilities</Text>
-                    </ListItem>
-                    <List
-                        dataArray={pokeData.abilities}
-                        renderRow={item => (
-                            <ListItem>
-                                <Text >{item.ability.name}</Text>
-                            </ListItem>
-                        )}
-                        >
+            <ImageBackground 
+                style={styles.backgroundImage}
+                source={{uri: "http://pokemongolive.com/img/posts/raids_loading.png"}}
+                >
+                <ScrollView style={{flex: 1}}>
+                    <Text style={styles.header}>#{pokeData.id} - {pokeData.name.toUpperCase()}</Text>
+                    <View style={styles.viewStyle}>
+                        <Image 
+                            source={{uri: pokeData.sprites.front_default}}
+                            style={styles.img}
+                        />
+                    </View>
+                    <View style={styles.info}>
+                        <ListItem itemDivider>
+                            <Text style={{fontWeight: 'bold'}}>Size</Text>
+                        </ListItem>
+                        <ListItem >
+                            <Text >Weight - {pokeData.weight} kg</Text>
+                        </ListItem>
+                        <ListItem >
+                            <Text >Height - {pokeData.height / 10} m</Text>
+                        </ListItem>
+                        <ListItem itemDivider>
+                            <Text style={{fontWeight: 'bold'}}>Abilities</Text>
+                        </ListItem>
+                        <List
+                            dataArray={pokeData.abilities}
+                            renderRow={item => (
+                                <ListItem>
+                                    <Text >{item.ability.name}</Text>
+                                </ListItem>
+                            )}
+                            >
 
-                    </List>
-                </View>
-            </ScrollView>
+                        </List>
+                    </View>
+                </ScrollView>
+            </ImageBackground>
         )
     }
 }
 
 const styles = {
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+        height: height,
+        width: width
+    },
     header: {
         fontSize: 30,
         color: 'red',
