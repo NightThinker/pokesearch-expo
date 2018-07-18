7d36109aff8d163f21caeb961ccad0d5037415f8
jiraphon <jiraphon@homedottech.com>
2018-07-18 16:00:18 +0700

    Display more data

--- src/SearchBody.js
+++ src/SearchBody.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { ScrollView, Text, View} from 'react-native'
+import { ScrollView, Text, View, Image } from 'react-native'
+import { ListItem, List } from 'native-base';
 
 class SearchBody extends React.Component {
     render() {
@@ -10,6 +11,23 @@ class SearchBody extends React.Component {
         return (
             <ScrollView style={{flex: 1}}>
                 <Text style={styles.header}>#{pokeData.id} - {pokeData.name.toUpperCase()}</Text>
+                <View style={styles.viewStyle}>
+                    <Image 
+                        source={{uri: pokeData.sprites.front_default}}
+                        style={styles.img}
+                    />
+                </View>
+                <View style={styles.info}>
+                    <ListItem itemDivider>
+                        <Text style={{fontWeight: 'bold'}}>Size</Text>
+                    </ListItem>
+                    <ListItem itemDivider>
+                        <Text >Weight - {pokeData.weight} kg</Text>
+                    </ListItem>
+                    <ListItem itemDivider>
+                        <Text >Height - {pokeData.height} in</Text>
+                    </ListItem>
+                </View>
             </ScrollView>
         )
     }
@@ -20,6 +38,22 @@ const styles = {
         fontSize: 30,
         color: 'red',
         textAlign: 'center'
+    },
+    viewStyle: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    img: {
+        height: 200,
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    info: {
+        flex: 1,
+        backgroundColor: 'white',
+        opacity: 0.8
     }
 }
 
