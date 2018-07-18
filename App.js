889900ec96bda58339b26948f3f17775e3b233da
jiraphon <jiraphon@homedottech.com>
2018-07-18 14:03:42 +0700

    second component

--- App.js
+++ App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import { Platform, Text, View, ImageBackground } from 'react-native';
-import { Button } from 'native-base';
+import { Platform, View } from 'react-native';
 
-const myBackgroud = require('./assets/landing.jpg')
+import Landing from './src/Landing';
+
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ImageBackground source={myBackgroud } style={{width: '100%', height: '100%'}} >
-          <View style={styles.viewStyle}>
-            <Text style={styles.titleStyle}>Welcome to PokeSearch</Text>
-            <Button 
-              block={true}
-              style={styles.buttonStyle}
-              onPress={() => {}}
-            >
-              <Text style={styles.buttonText}>Start Searching...</Text>
-            </Button>
-          </View>
-        </ImageBackground>
+        <Landing />
       </View>
     );
   }
