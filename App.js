761b7736c2eff58f69133415e58349b8cf496175
jiraphon <jiraphon@homedottech.com>
2018-07-18 14:23:38 +0700

    simple route

--- App.js
+++ App.js
@@ -2,14 +2,36 @@ import React from 'react';
 import { Platform, View } from 'react-native';
 
 import Landing from './src/Landing';
+import Search from './src/Search'
 
 
 
 export default class App extends React.Component {
+
+  state = {
+    currentScreen: 'landing'
+  }
+
+  switchScreen = (currentScreen) => {
+    this.setState({currentScreen})
+  }
+
+  renderScreen = () => {
+    if (this.state.currentScreen === 'landing') {
+      return (
+        <Landing switchScreen={this.switchScreen}/>
+      )
+    } else if (this.state.currentScreen === 'search') {
+      return (
+        <Search />
+      )
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Landing />
+        {this.renderScreen()}
       </View>
     );
   }
